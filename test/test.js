aa32ac5806346ef5c728b3d907cd28baa4aa177a
Felix Rieseberg <felix@felixrieseberg.com>
2017-10-06 09:35:11 -0700

    :package: Update dependencies

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ const getIsQuietHours = require('../lib/index').getIsQuietHours
 const isQuietHours = getIsQuietHours()
 
 if (process.platform !== 'win32') {
-    assert.strictEqual(isQuietHours, false)
+  assert.strictEqual(isQuietHours, false)
 } else {
-    const ok = isQuietHours === false || isQuietHours === true
-    assert.ok(ok, true)
+  const ok = isQuietHours === false || isQuietHours === true
+  assert.ok(ok, true)
 }
